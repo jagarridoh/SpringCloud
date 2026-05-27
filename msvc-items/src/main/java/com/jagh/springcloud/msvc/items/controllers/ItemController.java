9bfead71ce154c8a9b89e669e4bbf4fe48e7ea89
justo <jgarrido@iies.es>
2026-05-27 18:32:28 +0200

    66 combinar anotacion @CircuitBreaker y @TimeLimiter

--- msvc-items/src/main/java/com/jagh/springcloud/msvc/items/controllers/ItemController.java
+++ msvc-items/src/main/java/com/jagh/springcloud/msvc/items/controllers/ItemController.java
@@ -7,11 +7,13 @@ import com.jagh.springcloud.msvc.items.models.Product;
 import com.jagh.springcloud.msvc.items.services.ItemService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,21 +27,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class ItemController {
-
+    
     private final Logger logger = LoggerFactory.getLogger(ItemController.class);
-
+    
     private final ItemService service;
     private final CircuitBreakerFactory cBreakerFactory;
-
+    
     public ItemController(@Qualifier("itemServiceWebClient") ItemService service, 
-            CircuitBreakerFactory cBreakerFactory) {
+    CircuitBreakerFactory cBreakerFactory) {
         this.cBreakerFactory = cBreakerFactory;
         this.service = service;
     }
-
+    
     @GetMapping
     public List<Item> list(@RequestParam(name = "name", required = false) String name,
-                           @RequestHeader(name = "token-request", required = false) String token) {
+    @RequestHeader(name = "token-request", required = false) String token) {
         System.out.println(name);
         System.out.println(token);
         return service.findAll();
@@ -50,7 +52,7 @@ public class ItemController {
         Optional<Item> itemOptional = cBreakerFactory.create("items").run(() -> service.findById(id), e -> {
             System.out.println(e.getMessage());
             logger.error(e.getMessage());
-
+            
             Product product = new Product();
             product.setCreatedAt(
                 LocalDate.now());
@@ -58,52 +60,82 @@ public class ItemController {
             product.setName("Camara Sony");
             product.setPrice(
                 500.00);
-            return Optional.of(new Item(product,
+                return Optional.of(new Item(product,
                     5)); 
         });
-
+                    
         if(itemOptional.isPresent()) {
             return ResponseEntity.ok(itemOptional.get());
         }
         //return ResponseEntity.notFound().build(); << correcto pero devuelve body vacio
         return ResponseEntity.status(404)
-                .body(Collections.singletonMap(
-                    "message",
-                    "No existe el producto en el microservicio msvc-products"));
+        .body(Collections.singletonMap(
+            "message",
+            "No existe el producto en el microservicio msvc-products"));
     }
-
+        
     @CircuitBreaker(name = "items", fallbackMethod = "getFallBackmethodProduct")
     @GetMapping("/details/{id}")
     public ResponseEntity<?> details2(@PathVariable Long id) {
         Optional<Item> itemOptional = service.findById(id);
-
-        if(itemOptional.isPresent()) {
+            
+       if(itemOptional.isPresent()) {
             return ResponseEntity.ok(itemOptional.get());
         }
         //return ResponseEntity.notFound().build(); << correcto pero devuelve body vacio
         return ResponseEntity.status(404)
+            .body(Collections.singletonMap(
+                "message",
+                "No existe el producto en el microservicio msvc-products"));
+    }
+            
+    @CircuitBreaker(name = "items", fallbackMethod = "getFallBackmethodProduct2")
+    @TimeLimiter(name = "items")
+    @GetMapping("/details2/{id}")
+    public CompletableFuture<?> details3(@PathVariable Long id) {
+        return CompletableFuture.supplyAsync(() -> {
+            Optional<Item> itemOptional = service.findById(id);
+            
+            if(itemOptional.isPresent()) {
+                return ResponseEntity.ok(itemOptional.get());
+            }
+            
+            return ResponseEntity.status(404)
                 .body(Collections.singletonMap(
                     "message",
                     "No existe el producto en el microservicio msvc-products"));
+        }); 
     }
-
+        
     public ResponseEntity<?> getFallBackmethodProduct(Throwable e) {
         System.out.println(e.getMessage());
         logger.error(e.getMessage());
-
+        
         Product product = new Product();
         product.setCreatedAt(
             LocalDate.now());
-        product.setId(1L);
-        product.setName("Camara Sony");
-        product.setPrice(
-            500.00);
-        return ResponseEntity.ok(new Item(product,
-            5));
-
-
-
-
+            product.setId(1L);
+            product.setName("Camara Sony");
+            product.setPrice(
+                500.00);
+                return ResponseEntity.ok(new Item(product,
+                    5));
     }
 
-}
+    public CompletableFuture<?> getFallBackmethodProduct2(Throwable e) {
+        return CompletableFuture.supplyAsync(() -> {
+            System.out.println(e.getMessage());
+            logger.error(e.getMessage());
+            
+            Product product = new Product();
+            product.setCreatedAt(
+                LocalDate.now());
+                product.setId(1L);
+                product.setName("Camara Sony");
+                product.setPrice(
+                    500.00);
+                    return ResponseEntity.ok(new Item(product,
+                        5));
+        });
+    }
+}                           
